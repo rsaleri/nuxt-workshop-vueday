8e3590fb217a5753af827f257b4cc85a96adcfa2
Roberto Saleri <roberto.saleri@copangroup.com>
2020-09-21 15:40:38 +0200

    magnificent logo added

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
-<h1>
-  Hello World
-</h1>
+  <div>
+    <img :src="logo" alt="My beautiful application">
+    <h1>
+      Hello World
+    </h1>
+  </div>
 </template>
 
 <script>
 export default {
-name: "index"
+name: "index",
+  data() {
+    return {
+      logo: require('@/assets/logo.png')
+    }
+  }
 }
 </script>
 
